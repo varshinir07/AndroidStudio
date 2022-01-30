9fece99909ae9ef3fb962e2110e7405a3418b9ee
varshinir07 <varshini.r.2019.cse@rajalakshmi.edu.in>
2022-01-30 16:55:53 +0530

    Hello

---
+++ ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int minteger = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void increaseInteger(View view) {
+        minteger = minteger +1;
+        display(minteger);
+    }
+    public void decreaseInteger(View view) {
+        minteger = minteger -1;
+        display(minteger);
+    }
+    private void display(int number) {
+        TextView displayInteger = (TextView)  findViewById(
+                R.id.integer_number);
+        displayInteger.setText("Integer: "+number);
+    }
+}
