a712bcb29d544f0accaa329ff92d98ded50be26e
varshinir07 <varshini.r.2019.cse@rajalakshmi.edu.in>
2022-05-05 23:07:21 +0530

    ex8

--- ex6/app/src/main/java/com/example/ex6/MainActivity.java
+++ ex6/app/src/main/java/com/example/ex6/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.ex6;
 
-import androidx.appcompat.app.AppCompatActivity;
+import  androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
--- ex8/app/src/main/java/com/example/ex8/MainActivity.java
+++ ex8/app/src/main/java/com/example/ex8/MainActivity.java
@@ -10,8 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.ex8.R;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button b;
